3b0d4446dcb1b5bce6bd6220c2fb93a69c5a8192
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-07-25 22:39:46 +0300

    build-tfa.sh: Use $WORKSPACE.
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- bin/build-tfa.sh
+++ bin/build-tfa.sh
@@ -2,6 +2,6 @@
 set -ex
 
 export CROSS_COMPILE=/opt/gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
-cd /home/buildslave/workspace/pfalcon-tf-a-priv/trusted-firmware-a
+cd ${WORKSPACE}/trusted-firmware-a
 make PLAT=fvp clean
 make PLAT=fvp bl1 -j3
