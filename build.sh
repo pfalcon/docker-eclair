46a4fff7b4ac6217ddaa4057b60df700b0387c88
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-07-28 20:39:03 +0300

    build.sh: Use production tag with private AWS ECR repo
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ if [ ! -e gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf.tar.xz ]; then
     wget https://developer.arm.com/-/media/Files/downloads/gnu/11.2-2022.02/binrel/gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf.tar.xz
 fi
 
-docker build -t eclair-linaro-tfa .
+docker build -t 987685672616.dkr.ecr.us-east-1.amazonaws.com/misra:eclair-linaro-tfa .
