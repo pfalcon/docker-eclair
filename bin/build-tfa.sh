9c92a9fd48fbc3cabe4b555cea1a6404c0d28af5
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-07-29 14:05:06 +0300

    build-tfa.sh: Use $MAKE_TARGET
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- bin/build-tfa.sh
+++ bin/build-tfa.sh
@@ -4,4 +4,4 @@ set -ex
 export CROSS_COMPILE=/opt/gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
 cd ${WORKSPACE}/trusted-firmware-a
 make PLAT=$1 clean
-make PLAT=$1 bl1 -j3
+make PLAT=$1 ${MAKE_TARGET} -j3
