1b19906bce6ddb7c151a92f885cb573db7350998
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-08-09 21:59:33 +0300

    analyze.sh: Collate summary_txt into full_txt
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -87,6 +87,9 @@ make_self_contained ${ECLAIR_OUTPUT_DIR}/../full_html
 
 ${ECLAIR_BIN_DIR}/eclair_report -db=${PROJECT_ECD} -summary_txt=${ECLAIR_OUTPUT_DIR}/../summary_txt
 ${ECLAIR_BIN_DIR}/eclair_report -db=${PROJECT_ECD} -full_txt=${ECLAIR_OUTPUT_DIR}/../full_txt
+# summary_txt contains just a single report file not present in full_txt, move it there and be done with it.
+mv ${ECLAIR_OUTPUT_DIR}/../summary_txt/by_service.txt ${ECLAIR_OUTPUT_DIR}/../full_txt/
+rm -rf ${ECLAIR_OUTPUT_DIR}/../summary_txt
 
 # Create the Jenkins reports file.
 JENKINS_XML="${ECLAIR_OUTPUT_DIR}/../jenkins.xml"
