65ab583ccb1dde7fa09801f9e9058f88e8a90e6d
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-08-10 15:55:42 +0300

    analyze.sh: Generate summary_txt and full_txt in one go
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -83,8 +83,7 @@ function make_self_contained() {
 ${ECLAIR_BIN_DIR}/eclair_report -db=${PROJECT_ECD} -full_html=${ECLAIR_OUTPUT_DIR}/../full_html
 make_self_contained ${ECLAIR_OUTPUT_DIR}/../full_html
 
-${ECLAIR_BIN_DIR}/eclair_report -db=${PROJECT_ECD} -summary_txt=${ECLAIR_OUTPUT_DIR}/../summary_txt
-${ECLAIR_BIN_DIR}/eclair_report -db=${PROJECT_ECD} -full_txt=${ECLAIR_OUTPUT_DIR}/../full_txt
+${ECLAIR_BIN_DIR}/eclair_report -db=${PROJECT_ECD} -summary_txt=${ECLAIR_OUTPUT_DIR}/../summary_txt -full_txt=${ECLAIR_OUTPUT_DIR}/../full_txt
 # summary_txt contains just a single report file not present in full_txt, move it there and be done with it.
 mv ${ECLAIR_OUTPUT_DIR}/../summary_txt/by_service.txt ${ECLAIR_OUTPUT_DIR}/../full_txt/
 rm -rf ${ECLAIR_OUTPUT_DIR}/../summary_txt
