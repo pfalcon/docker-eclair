3b96c15de35fdf0deb835660f2a9e6048779b987
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-08-10 16:38:35 +0300

    analyze_delta.sh: Compress project databases before archiving them
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- bin/analyze_delta.sh
+++ bin/analyze_delta.sh
@@ -120,4 +120,6 @@ diff -I '^Timestamp:' -x frames.txt -x files.txt -x explain.txt \
 cd ${ECLAIR_OUTPUT_DIR}/..
 ${ECLAIR_BIN_DIR}/eclair_report -diff_criteria=fingerprint -diff_full_txt=../ECLAIR_BASE/out/PROJECT.ecd,out/PROJECT.ecd
 ls -l diff_output
+
+xz ../ECLAIR_BASE/out/PROJECT.ecd out/PROJECT.ecd
 )
