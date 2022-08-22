c25a3d1379e0e9d93a2bf5a7cde80a0dc7e493db
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-08-22 16:57:05 +0300

    post_gerrit_comment.sh: Script to post analysis report to Gerrit patch
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

---
+++ bin/post_gerrit_comment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+set -ex
+
+should_post_comment=0
+
+if echo "${GERRIT_PROJECT}" | grep -q sandbox; then
+    should_post_comment=1
+fi
+
+if [ $should_post_comment -eq 1 ]; then
+    ssh -p 29418 -i "$CI_BOT_KEY" "$CI_BOT_USERNAME@$GERRIT_HOST" gerrit \
+        review  "$GERRIT_CHANGE_NUMBER,$GERRIT_PATCHSET_NUMBER" \
+        --message "'$(cat misra_delta.txt)'"
+fi
