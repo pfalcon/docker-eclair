6e322b180b118447f131887dfd33a5286ced837d
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-08-23 21:29:27 +0300

    post_gerrit_comment.sh: Robust quoting of file contents
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- bin/post_gerrit_comment.sh
+++ bin/post_gerrit_comment.sh
@@ -15,7 +15,10 @@ fi
 if [ $should_post_comment -eq 1 ]; then
     mkdir -p ~/.ssh/
     ssh-keyscan -H -p 29418 $GERRIT_HOST >> ~/.ssh/known_hosts
+
+    quoted="$(python3 -c 'import sys, shlex; print(shlex.quote(open(sys.argv[1]).read()))' misra_delta.txt)"
+
     ssh -vvvv -o "PubkeyAcceptedKeyTypes +ssh-rsa" -p 29418 -i "$CI_BOT_KEY" "$CI_BOT_USERNAME@$GERRIT_HOST" gerrit \
         review  "$GERRIT_CHANGE_NUMBER,$GERRIT_PATCHSET_NUMBER" \
-        --message "'$(cat misra_delta.txt)'"
+        --message "$quoted"
 fi
