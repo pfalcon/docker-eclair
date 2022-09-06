c6f5b34238147a6cd2db94318da51f404580a896
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2022-09-06 16:27:04 +0300

    bin: Analysis-related scripts move to another repo
    https://git.trustedfirmware.org/next/ci/tf-a-ci-scripts.git/ , eclair/
    subdir.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- bin/build-tfa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ex
-
-export CROSS_COMPILE=/opt/gcc-arm-11.2-2022.02-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
-cd ${WORKSPACE}/trusted-firmware-a
-make PLAT=$1 clean
-make PLAT=$1 ${MAKE_TARGET} -j3
--- bin/public_APIs.ecl
+++
@@ -1,5 +0,0 @@
-# Definition of the public APIs.
-
-
--doc="FIXME: add proper documentation."
--file_tag+={api:public,"^include/arch/aarch64/arch\\.h$"}
